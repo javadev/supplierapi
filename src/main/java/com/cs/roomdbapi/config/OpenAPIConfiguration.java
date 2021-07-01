6f74e12fb53527e68f2641714b5891aeaab9040c
Alex Mikhashchuk <alex.mikhashchuk@gmail.com>
2021-07-01 17:00:45 +0300

    Merge OpenAPI configs and update snapshot version.

--- src/main/java/com/cs/roomdbapi/config/OpenAPIConfiguration.java
+++ src/main/java/com/cs/roomdbapi/config/OpenAPIConfiguration.java
@@ -1,11 +1,28 @@
 package com.cs.roomdbapi.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Supplier API.",
+                version = "v1",
+                description = "Provides an API endpoints to access data on RoomDB."
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class OpenAPIConfiguration {
 
     @Bean
