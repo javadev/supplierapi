932d10c31ec8396d56a20b058ec5c44af5eb13c9
Alex Mikhashchuk <alex.mikhashchuk@gmail.com>
2021-07-09 14:04:53 +0300

    Add  NotificationSupplier statuses.

--- src/main/java/com/cs/roomdbapi/utilities/AppUtils.java
+++ src/main/java/com/cs/roomdbapi/utilities/AppUtils.java
@@ -6,8 +6,18 @@ public class AppUtils {
     public static final int RESPONSE_CODE_NO_DATA = 102;
     public static final int RESPONSE_CODE_FAIL = 103;
 
+    public static final short NOTIFICATION_SUPPLIER_STATUS_QUEUED = 0;
+    public static final short NOTIFICATION_SUPPLIER_STATUS_IN_PROGRESS = 1;
+    public static final short NOTIFICATION_SUPPLIER_STATUS_FINISHED = 2;
+    public static final short NOTIFICATION_SUPPLIER_STATUS_FAILED = 10;
+    public static final short NOTIFICATION_SUPPLIER_STATUS_FAILED_NO_URL = 11;
+    public static final short NOTIFICATION_SUPPLIER_STATUS_FAILED_NO_EMAIL = 12;
+
     public static final String RESPONSE_CODE_SUCCESS_MSG = "Successful operation";
     public static final String RESPONSE_CODE_NO_DATA_MSG = "No data found";
+
+    public static final String TEST_NOTIFICATION_SUCCESS = "Successful sending test notification";
+    public static final String TEST_NOTIFICATION_FAIL = "Failed to sending test notification";
 
     public static final String FAIL = "fail";
     public static final String SUCCESS = "success";
