03f76d752356785b295f47082d73f7110c373a9a
Alex Mikhashchuk <alex.mikhashchuk@gmail.com>
2021-07-15 16:05:00 +0300

    - BadRequestException remove Throwable param in constructor.

--- src/main/java/com/cs/roomdbapi/exception/BadRequestException.java
+++ src/main/java/com/cs/roomdbapi/exception/BadRequestException.java
@@ -17,8 +17,8 @@ public class BadRequestException extends RuntimeException {
 
     private Object object;
 
-    public BadRequestException(String message, Throwable cause) {
-        super(message, cause);
+    public BadRequestException(String message) {
+        this.message = message;
     }
 
 }
