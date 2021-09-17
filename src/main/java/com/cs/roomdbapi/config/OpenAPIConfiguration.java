0e94903a723d168a978dd363fc94bdfe8623b2c3
Alex Mikhashchuk <alex.mikhashchuk@gmail.com>
2021-09-17 16:17:30 +0300

    Swagger and ReDoc - show build version from pom.xml

--- src/main/java/com/cs/roomdbapi/config/OpenAPIConfiguration.java
+++ src/main/java/com/cs/roomdbapi/config/OpenAPIConfiguration.java
@@ -1,24 +1,15 @@
 package com.cs.roomdbapi.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Supplier API.",
-                version = "v1",
-                description = "Provides an API endpoints to access data on RoomDB. <br/>" +
-                        "More detailed information can be found in project " +
-                        "<a href=\"https://repo.cultuzz.com/roomdb/supplierapi/-/blob/master/README.md\">README file</a> <br/>"
-        )
-)
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
@@ -27,9 +18,23 @@ import org.springframework.context.annotation.Configuration;
 )
 public class OpenAPIConfiguration {
 
+    @Value("${project.version}")
+    String version;
+
+    String title = "Supplier API.";
+
+    String description = "Provides an API endpoints to access data on RoomDB. <br/>" +
+            "More detailed information can be found in project " +
+            "<a href=\"https://repo.cultuzz.com/roomdb/supplierapi/-/blob/master/README.md\">README file</a> <br/>";
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .addServersItem(new Server().url("/"));
+                .addServersItem(new Server().url("/"))
+                .info(new Info()
+                        .title(title)
+                        .version(version)
+                        .description(description)
+                );
     }
 }
